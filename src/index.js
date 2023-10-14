e2da41a89638cb77075572c7653baca318e39c97
gnocchi <gkussunoki@gmail.com>
2023-10-14 13:18:45 -0300

    Create a separate module for createCard

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
+import createCard from './card';
 import initialMenu from '../dist/initial-menu.json';
-
-function createCard(item) {
-  const card = document.createElement('div');
-  const img = document.createElement('img');
-  const text = document.createElement('div');
-  const name = document.createElement('h2');
-  const description = document.createElement('p');
-  const shoutout = document.createElement('p');
-
-  card.classList.add('card');
-  text.classList.add('text');
-  name.classList.add('name');
-  description.classList.add('description');
-  shoutout.classList.add('shoutout');
-
-  card.appendChild(img);
-  card.appendChild(text);
-  text.appendChild(name);
-  text.appendChild(description);
-  text.appendChild(shoutout);
-
-  img.src = item.img;
-  name.textContent = item.name;
-  description.textContent = item.description;
-  shoutout.innerHTML = item.shoutout;
-
-  return card;
-}
 
 function populateMenu(items) {
   const menu = document.querySelector('.menu');
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,19 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    card: './src/card.js',
+  },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
