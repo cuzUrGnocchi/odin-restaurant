d9c0b8e4ab172a0deb134fe6c80db87f7be80acf
gnocchi <gkussunoki@gmail.com>
2023-10-14 14:03:16 -0300

    Add a title

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import createMenu from './menu';
-import initialMenu from '../dist/initial-menu.json';
+import initialMenu from './initial-menu.json';
 
 function init() {
+  const title = document.createElement('h1');
   const menu = createMenu(initialMenu.menu);
+
+  document.body.appendChild(title);
   document.body.appendChild(menu);
+
+  title.classList.add('title');
+  title.textContent = 'Food';
 }
 
 init();
